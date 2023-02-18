07774372c94792f266d4003f9570858117fe9cb7
Austin Milt <austin@holaplex.com>
2023-02-17 22:50:19 -0500

    add mantine

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,14 @@
 import { NextPage } from 'next';
 import Link from 'next/link';
 import { useApolloClient } from '@apollo/client';
+import { Demo } from '../components/Table';
 
 const IndexPage: NextPage = () => {
   const apolloClient = useApolloClient();
 
   return (
     <section>
-      <h1>Hello, you're using Apollo Client {apolloClient.version}</h1>
-      <p>Here are few examples of how to use it:</p>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/users-csr">Client-Side Rendering</Link>
-          </li>
-          <li>
-            <Link href="/users-ssr">Server-Side Rendering</Link>
-          </li>
-        </ul>
-      </nav>
+      <Demo />
     </section>
   );
 };
